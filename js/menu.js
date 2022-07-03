9443636fdaabcc6bc44725f3729558ed90c53934
Andrew-aka-Lemon <105217029+Andrew-aka-Lemon@users.noreply.github.com>
2022-07-03 20:35:08 +0300

    mobile menu looks like is done)

--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,7 @@
 (() => {
   const refs = {
-    openMenuBtn: document.querySelector(".menu-open-btn"),
-    closeMenuBtn: document.querySelector(".menu-close-btn"),
+    openMenuBtn: document.querySelector(".mob-menu-btn"),
+    closeMenuBtn: document.querySelector(".mob-menu-btn"),
     menu: document.querySelector(".mob-menu"),
     body: document.querySelector("body"),
   };
@@ -10,7 +10,7 @@
   refs.closeMenuBtn.addEventListener("click", toggleMenu);
 
   function toggleMenu() {
-    refs.menu.classList.toggle("hidden");
+    refs.menu.classList.toggle("is-hidden");
     refs.body.classList.toggle("no-scroll");
   }
 })();
